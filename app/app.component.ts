c8555d1271001702e1778f2bc5e923e36faede28
brooke hurford and ryan brown <brookebhurford@gmail.com>
2016-03-23 08:52:18 -0700

    Show three hardcoded keg types

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from 'angular2/core';
+import { Keg } from './keg.model';
 
 @Component({
   selector: 'my-app',
   template: `
     <div class="container">
-      <h1>Skeleton Angular2 App!</h1>
+      <h1>Welcome to Bailey's Taproom!</h1>
+      <h3 *ngFor="#keg of kegs">{{ keg.name }} <em>{{ keg.brand }}</em> {{ keg.alcoholContent }}% -- {{ keg.price }} <br>Pints Left: {{ keg.pintsLeft }}</h3>
     <div>
   `
 })
 export class AppComponent {
-
+  public kegs: Keg[];
+  constructor(){
+    this.kegs = [
+      new Keg("Winchester Brown", "Backwoods", 6.2, "$4", 124),
+      new Keg("IPA 97", "Silver Moon", 7.0, "$4", 124),
+      new Keg("Noggin’ Floggin'", "Hopworks 2011", 11.0, "$8", 124)
+    ];
+  }
 }
---
+++ app/keg.model.ts
@@ -0,0 +1,5 @@
+// Model
+export class Keg {
+  constructor(public name: string, public brand: string, public alcoholContent: number, public price: string, public pintsLeft: number){
+  }
+}
