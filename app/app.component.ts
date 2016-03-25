e49e06a50410a22ef1d5f314361217488a0a92ef
Brooke Hurford <brookebhurford@gmail.com>
2016-03-24 19:45:18 -0700

    Update styles to be coming from sass file instead of css

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,26 +1,21 @@
 import { Component, EventEmitter } from 'angular2/core';
-import { Keg } from './keg.model';
-import { KegListComponent } from './keg-list.component';
-import { NewKegComponent } from './new-keg.component';
-import { KegComponent } from './keg.component';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
-  directives: [KegListComponent],
   template: `
     <div class="container">
-      <h1>Welcome to Bailey's Taproom!</h1>
-      <keg-list [kegList]="kegs"></keg-list>
+      <h1>Meal Tracker</h1>
     </div>
   `
 })
 export class AppComponent {
-  public kegs: Keg[];
+  public meals: Meal[];
   constructor(){
-    this.kegs = [
-      new Keg("Winchester Brown", "Backwoods", 6.2, 4, 11),
-      new Keg("IPA 97", "Silver Moon", 7.0, 4 , 124),
-      new Keg("Noggin’ Floggin'", "Hopworks 2011", 11.0, 8, 124)
+    this.meals = [
+      new Meal("Tacos", "Fish tacos, with no cheese", 450),
+      new Meal("Chicken and Asparagus", "Cooked at home", 600),
+      new Meal("Kind Granola Bar", "Sugar, only 5 grams", 100)
     ];
   }
 }
