87f4f75f6cd912e1a1f3d82b8403689ac98b81fd
Brooke Hurford <brookebhurford@gmail.com>
2016-03-24 21:16:04 -0700

    Add edit meal component, but is not working yet

---
+++ app/edit-meal.component.ts
@@ -0,0 +1,18 @@
+import { Component, EventEmitter } from 'angular2/core';
+import { Meal } from './meal.model';
+
+@Component({
+  selector: 'edit-keg',
+  inputs: ['meal'],
+  template: `
+  <div class="editMeal">
+  <h3>Edit Meal Details:</h3>
+  <input [(ngModel)]="meal.name"/>
+  <input [(ngModel)]="meal.details"/>
+  <input [(ngModel)]="meal.calories"/>
+  </div>
+  `
+})
+export class EditMealComponent {
+  public meal: Meal;
+}
--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,27 +1,39 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 import { NewMealComponent } from './new-meal.component';
+import { EditMealComponent } from './edit-meal.component';
 
 @Component({
   selector: 'meal-list',
   inputs: ['mealList'],
-  directives: [NewMealComponent],
+  directives: [NewMealComponent, EditMealComponent],
   template: `
-  <div *ngFor="#meal of mealList">
+  <div *ngFor="#meal of mealList"
+  (click)="mealClicked(meal)"
+  [class.selected]="meal === selectedMeal">
     <p>Name: {{ meal.name }}</p>
     <p>Details: {{ meal.details }}</p>
     <p>Calories: {{ meal.calories }}</p>
     <hr>
   </div>
+  <edit-meal *ngIf="selectedMeal" [meal]="selectedMeal">
+  </edit-meal>
   <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
   `
 })
 export class MealListComponent {
   public mealList: Meal[];
+  public onMealSelect: EventEmitter<Meal>;
+  public selectedMeal: Meal;
   createMeal(newMealInfo): void {
     console.log(newMealInfo);
     this.mealList.push(
       new Meal(newMealInfo[0], newMealInfo[1], newMealInfo[2])
     );
+  }
+  mealClicked(clickedMeal: Meal): void {
+    console.log(clickedMeal);
+    this.selectedMeal = clickedMeal;
+    this.onMealSelect.emit(clickedMeal);
   }
 }
